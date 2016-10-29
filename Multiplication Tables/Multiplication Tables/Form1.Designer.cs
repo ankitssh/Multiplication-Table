46c68571ef90e52e5feafa17a5a99962f05e610b
CTOS\Ankit <godotcoffee231@gmail.com>
2016-10-29 11:24:11 +0530

    no message

---
+++ Multiplication Tables/Multiplication Tables/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Multiplication_Tables
+{
+    partial class displayTablebox
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(displayTablebox));
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.printBtn = new System.Windows.Forms.Button();
+            this.getNumBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(50, 197);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(273, 196);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // printBtn
+            // 
+            this.printBtn.Location = new System.Drawing.Point(150, 152);
+            this.printBtn.Name = "printBtn";
+            this.printBtn.Size = new System.Drawing.Size(75, 23);
+            this.printBtn.TabIndex = 1;
+            this.printBtn.Text = "Print";
+            this.printBtn.UseVisualStyleBackColor = true;
+            this.printBtn.Click += new System.EventHandler(this.printBtn_Click);
+            // 
+            // getNumBox
+            // 
+            this.getNumBox.Location = new System.Drawing.Point(177, 90);
+            this.getNumBox.Name = "getNumBox";
+            this.getNumBox.Size = new System.Drawing.Size(100, 22);
+            this.getNumBox.TabIndex = 2;
+            this.getNumBox.TextChanged += new System.EventHandler(this.getNumBox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 90);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter the number";
+            // 
+            // displayTablebox
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.ClientSize = new System.Drawing.Size(380, 405);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.getNumBox);
+            this.Controls.Add(this.printBtn);
+            this.Controls.Add(this.listBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "displayTablebox";
+            this.Text = "Tables";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button printBtn;
+        private System.Windows.Forms.TextBox getNumBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Multiplication Tables/Multiplication Tables/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Multiplication_Tables
+{
+    public partial class displayTablebox : Form
+    {
+        public displayTablebox()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void printBtn_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+            try
+            {
+                int num = int.Parse(getNumBox.Text);
+
+                for (int i = 1; i <= 10; i++)
+                {
+                    listBox1.Items.Add(getNumBox.Text + " times " + i + " = " + (num * i).ToString());
+
+                }
+
+            }
+            catch(Exception)
+            {
+                MessageBox.Show("Invalid input. Please enter a valid input","Error",MessageBoxButtons.OK,MessageBoxIcon.Error);
+            }
+        }
+        private void getNumBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
